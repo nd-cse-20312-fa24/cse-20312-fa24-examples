420774f1a7e975e9733648fbd47f59148878fb13
Jay Brockman <jbb@nd.edu>
2024-10-11 10:18:40 -0400

    Working on parse_demo

--- ex24_py_parse/parse_demo.py
+++ ex24_py_parse/parse_demo.py
@@ -7,20 +7,20 @@ def main():
     input = sys.stdin.read().strip()
 
     # Split the input into a list of strings
-    number_str = input.split()
+    number_str_list = input.split()
 
     # Convert the list of strings into a list of integers
     # Note: This is a list comprehension
-    numbers = [int(num) for num in number_str]
+    numbers = [int(num) for num in number_str_list]
 
     # Increment each number by 1
     incremented_numbers = [num + 1 for num in numbers]
 
     # Convert the list of integers into a list of strings
-    incremented_numbers_str = [str(num) for num in incremented_numbers]
+    incremented_numbers_str_list = [str(num) for num in incremented_numbers]
 
     # Join the list of strings into a single string with commas and spaces
-    print(', '.join(incremented_numbers_str))
+    print(', '.join(incremented_numbers_str_list))
 
 if __name__ == "__main__":
     main()
